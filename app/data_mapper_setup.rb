3bf583d05472a8169150862ba7995eb50dc0f660
Ben <ben.c.hawker@gmail.com>
2015-08-27 18:35:28 +0100

    Heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,7 +4,8 @@ require 'dm-migrations'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/chitter_#{env}")
+DataMapper.setup(:default,
+                 ENV["DATABASE_URL"] || "postgres://localhost/chitter_#{env}")
 
 require_relative './models/user'
 require_relative './models/peep' 
