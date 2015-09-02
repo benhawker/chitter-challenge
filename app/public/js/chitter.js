ee9b382dd68aecb45a6b73b51f6c2cfc7c5deb9f
Ben <ben.c.hawker@gmail.com>
2015-09-02 09:20:09 +0100

    Small js changed

--- app/public/js/chitter.js
+++ app/public/js/chitter.js
@@ -4,7 +4,7 @@ $('#tweetField').keyup(function () {
   var max = 240;
   var len = $(this).val().length;
   if (len >= max) {
-    $('#counter').text(' you have reached the limit');
+    $('#counter').text('Character count reached');
   } else {
     var char = max - len;
     $('#counter').text(char + ' characters left');
